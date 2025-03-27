547558446d4ec9b57a72e70778b72fd8cbbc74ce
Kei-TAKAHATA <keitakahataresearch@gmail.com>
2025-03-27 09:51:11 +0900

    [feature]出力するファイル名にモデル名を追加

--- time_series_forecasting/utils/data_manager.py
+++ time_series_forecasting/utils/data_manager.py
@@ -23,6 +23,7 @@ class DataManager:
             os.chmod(output_dir_path, 0o777)
         self.train_data_name = settings["data"]["train_data_name"]
         self.test_data_name = settings["data"]["test_data_name"]
+        self.model_name = settings["model"]["model_name"]
         self.output_file_base_name = settings["data"]["output_file_base_name"]
 
     def load_train_and_test_data(self):
@@ -36,9 +37,9 @@ class DataManager:
         """
         train_file_path = os.path.join(self.root_dir, self.datasets_dir, self.input_dir, self.train_data_name)
         test_file_path = os.path.join(self.root_dir, self.datasets_dir, self.input_dir, self.test_data_name)
-        train_df = pd.read_csv(train_file_path, encoding="utf-8")
-        test_df = pd.read_csv(test_file_path, encoding="utf-8")
-        return train_df, test_df
+        train_data = pd.read_csv(train_file_path, encoding="utf-8")
+        test_data = pd.read_csv(test_file_path, encoding="utf-8")
+        return train_data, test_data
 
     def save_df_data(self, df, prefix="", suffix=""):
         """
@@ -53,9 +54,9 @@ class DataManager:
         suffix : str, optional
             ファイル名の接尾辞。
         """
-        # 日付_時間_ベース名_追加オプション.csv
-        file_name = f"{prefix}_{self.output_file_base_name}_{suffix}.csv"
+        # 日付_時間_モデル名_ベース名_追加オプション.csv
+        file_name = f"{prefix}_{self.model_name}_{self.output_file_base_name}_{suffix}.csv"
         file_path = os.path.join(self.root_dir, self.datasets_dir, self.output_dir, file_name)
-        df.to_csv(file_path, index=False)
+        df.to_csv(file_path, index=True)
         # 権限変更
         os.chmod(file_path, 0o777)
